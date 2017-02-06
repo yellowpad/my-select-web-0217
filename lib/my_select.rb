4b1dbab793fef67862b59d37ebd867d1927fce4c
Zayne Abraham <zayneabraham1@gmail.com>
2017-02-06 04:10:37 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,23 @@
+
+
 def my_select(collection)
  # your code here!
+ if collection.length < 1
+   puts "This block should not run!"
+ else
+   array = []
+   i = 0
+   while i < collection.length
+     if yield collection[i]
+       array.push(collection[i])
+     end
+     i += 1
+   end
+ end
+ array
+end
+
+col = (1..5).to_a
+my_select(col) do |n|
+  n.even?
 end
